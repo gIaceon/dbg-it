8becf663dc28a48b521f3e00d8f0915da65d76f5
gIaceon <16375167+gIaceon@users.noreply.github.com>
2024-04-11 18:37:17 -0400

    remove stack traces from dbg-it errors

--- src/client/exec.ts
+++ src/client/exec.ts
@@ -7,14 +7,25 @@ export class ExecClient extends BaseExec {
 	public constructor(dbgit: DbgIt) {
 		super(dbgit);
 	}
-	public async execute(commandString: string) {
-		const split = splitString(commandString, "%s");
-		const commandName = split[0];
-		if (this.dbgit.registry.getCommands().has(commandName) === false) {
-			const { success, result } = remotes.executeCommand(commandString).expect();
-			if (!success) throw tostring(result ?? "Unknown error!");
-			return result;
-		}
-		return super.execute(commandString, game.GetService("Players").LocalPlayer);
+	public execute(commandString: string): ReturnType<BaseExec["execute"]> {
+		return new Promise((resolve, reject) => {
+			const split = splitString(commandString, "%s");
+			const commandName = split[0];
+			if (this.dbgit.registry.getCommands().has(commandName) === false) {
+				const { success, result } = remotes.executeCommand(commandString).expect();
+				if (!success) reject(tostring(result ?? "Unknown error!"));
+				return resolve(result);
+			}
+			let result: string | void | undefined = undefined;
+			let doThrow = false;
+			super
+				.execute(commandString, game.GetService("Players").LocalPlayer)
+				.andThen((r) => (result = r))
+				.catch((e) => {
+					result = e;
+					doThrow = true;
+				});
+			return doThrow ? reject(result) : resolve(result);
+		});
 	}
 }
--- src/shared/class/exec.ts
+++ src/shared/class/exec.ts
@@ -7,73 +7,92 @@ import { DbgIt } from "./dbgItBase";
 export abstract class BaseExec {
 	protected constructor(public readonly dbgit: DbgIt) {}
 
-	public async runHooksOf(injectionPoint: HookInjectPoints, ...args: Parameters<HookFunction>) {
-		let res: string | undefined = undefined;
-		this.dbgit.registry.getHooksOf(injectionPoint).forEach((v) => {
-			if (res !== undefined) return;
-			const hookResult = v.hook(...args);
-			if (hookResult !== undefined) res = hookResult;
+	public runHooksOf(injectionPoint: HookInjectPoints, ...args: Parameters<HookFunction>): Promise<boolean> {
+		return new Promise((resolve, reject) => {
+			let res: string | undefined = undefined;
+			this.dbgit.registry.getHooksOf(injectionPoint).forEach((v) => {
+				if (res !== undefined) return;
+				const hookResult = v.hook(...args);
+				if (hookResult !== undefined) res = hookResult;
+			});
+			if (res) reject(res);
+			return resolve(true);
 		});
-		if (res) throw res;
-		return true;
 	}
 
-	public async execute(commandString: string, executor: Player) {
-		const split = splitString(commandString, "%s");
-		const commandName = split[0];
-		if (!this.dbgit.registry.getCommands().has(commandName)) throw `No command ${commandName}`;
-		const command = this.dbgit.registry.getCommands().get(commandName)!;
-		split.remove(0);
-		const ctx: CommandCtx = {
-			Executor: executor,
-			RawCommandString: commandString,
-			RawArguments: split,
-			CommandName: commandName,
-			Group: command.getDef().Group,
-			CommandDefinition: command.getDef(),
-		};
-		const args = split.mapFiltered((v, i) => {
-			if (command.getArgs() === undefined) return undefined;
-			const currentArgument = command.getArgs()!.getArguments()[i];
-			if (!currentArgument) return;
-			const result = currentArgument.transform(v, ctx);
-			if (currentArgument.validate(result)) return result;
-			throw `Invalid argument ${v} (expected ${currentArgument.getName()})`;
-		});
-		// We now need to remove all optional ending arguments from the array to ensure the size is correct.
-		if (
-			command.getArgs() &&
-			split.size() <
-				command
-					.getArgs()!
-					.getArgumentDef()
-					.mapFiltered((v) => (v.Optional ? undefined : v))
-					.size()
-		) {
-			throw `Expected ${
-				command
-					.getArgs()
-					?.getArgumentDef()
-					.mapFiltered((v) => v.Optional)
-					.reduce((accumulator, isOptional) => {
-						// Determine if any arguments of the array are optional
-						if (accumulator === true) return accumulator;
-						return isOptional;
-					})
-					? "at least "
-					: ""
-			}${
-				command
-					.getArgs()
-					?.getArgumentDef()
-					.mapFiltered((v) => (v.Optional ? undefined : v))
-					.size() ?? 0
-			} argument(s), got ${split.size()}`;
-		}
+	public execute(commandString: string, executor: Player): Promise<string | void | undefined> {
+		return new Promise((resolve, reject) => {
+			const split = splitString(commandString, "%s");
+			const commandName = split[0];
+			if (!this.dbgit.registry.getCommands().has(commandName)) return reject(`No command ${commandName}`);
+			const command = this.dbgit.registry.getCommands().get(commandName)!;
+			split.remove(0);
+			const ctx: CommandCtx = {
+				Executor: executor,
+				RawCommandString: commandString,
+				RawArguments: split,
+				CommandName: commandName,
+				Group: command.getDef().Group,
+				CommandDefinition: command.getDef(),
+			};
+			let possibleErr: string | undefined;
+			const args = split.mapFiltered((v, i) => {
+				if (possibleErr !== undefined) return undefined;
+				if (command.getArgs() === undefined) return undefined;
+				const currentArgument = command.getArgs()!.getArguments()[i];
+				if (!currentArgument) return;
+				const result = currentArgument.transform(v, ctx);
+				if (currentArgument.validate(result)) return result;
+				possibleErr = `Invalid argument ${v} (expected ${currentArgument.getName()})`;
+			});
+			if (possibleErr !== undefined) return reject(possibleErr);
+			// We now need to remove all optional ending arguments from the array to ensure the size is correct.
+			if (
+				command.getArgs() &&
+				split.size() <
+					command
+						.getArgs()!
+						.getArgumentDef()
+						.mapFiltered((v) => (v.Optional ? undefined : v))
+						.size()
+			) {
+				return reject(
+					`Expected ${
+						command
+							.getArgs()
+							?.getArgumentDef()
+							.mapFiltered((v) => v.Optional)
+							.reduce((accumulator, isOptional) => {
+								// Determine if any arguments of the array are optional
+								if (accumulator === true) return accumulator;
+								return isOptional;
+							})
+							? "at least "
+							: ""
+					}${
+						command
+							.getArgs()
+							?.getArgumentDef()
+							.mapFiltered((v) => (v.Optional ? undefined : v))
+							.size() ?? 0
+					} argument(s), got ${split.size()}`,
+				);
+			}
 
-		this.runHooksOf(HookInjectPoints.BeforeExecution, ctx).expect();
-		const result = command.run(ctx, ...args);
-		this.runHooksOf(HookInjectPoints.AfterExecution, ctx).expect();
-		return result;
+			let err: string | undefined = undefined;
+
+			this.runHooksOf(HookInjectPoints.BeforeExecution, ctx)
+				.catch((e: string) => (err = e))
+				.await();
+			if (err !== undefined) return reject(err);
+
+			const result = command.run(ctx, ...args);
+
+			this.runHooksOf(HookInjectPoints.AfterExecution, ctx)
+				.catch((e: string) => (err = e))
+				.await();
+			if (err !== undefined) return reject(err);
+			return resolve(result);
+		});
 	}
 }
