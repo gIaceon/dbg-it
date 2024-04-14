c40793b813814819ef078fa1c4e748099aa1fb11
gIaceon <16375167+gIaceon@users.noreply.github.com>
2024-04-14 17:04:20 -0400

    add event param to mw log fn, getSeverityLabel

--- src/shared/class/logging.ts
+++ src/shared/class/logging.ts
@@ -3,10 +3,15 @@ import { MessageTemplateParser, PlainTextMessageTemplateRenderer } from "@rbxts/
 
 const MESSAGE_HISTORY_MAX = 1000;
 
-export type LoggingMiddleware = (severity: LogLevel, time: string | undefined, message: string) => void;
+export type LoggingMiddleware = (
+	severity: LogLevel,
+	time: string | undefined,
+	message: string,
+	event: LogEvent,
+) => void;
 
 export class DbgItLoggingSink implements ILogEventSink {
-	private history: { severity: LogLevel; time: string | undefined; message: string }[] = [];
+	private history: { severity: LogLevel; time: string | undefined; message: string; event: LogEvent }[] = [];
 	private middleware: LoggingMiddleware[];
 	constructor(...middleware: LoggingMiddleware[]) {
 		this.middleware = middleware;
@@ -14,8 +19,23 @@ export class DbgItLoggingSink implements ILogEventSink {
 	Emit(message: LogEvent): void {
 		const template = new PlainTextMessageTemplateRenderer(MessageTemplateParser.GetTokens(message.Template));
 		const time = DateTime.fromIsoDate(message.Timestamp)?.FormatLocalTime("HH:mm:ss", "en-us");
+		const tag = this.getSeverityLabel(message.Level);
+		const renderedMessage = template.Render(message);
+		const formattedMessage = `[${tag!}]: ${renderedMessage}`;
+		this.middleware.forEach((mw) => coroutine.wrap(mw)(message.Level, time, formattedMessage, message));
+		this.history.push({
+			severity: message.Level,
+			time: time,
+			message: formattedMessage,
+			event: message,
+		});
+		this.history = this.history.filter(
+			(_, i) => this.history.size() > MESSAGE_HISTORY_MAX && i <= this.history.size() - MESSAGE_HISTORY_MAX,
+		);
+	}
+	public getSeverityLabel(level: LogLevel) {
 		let tag: string | undefined = undefined;
-		switch (message.Level) {
+		switch (level) {
 			case LogLevel.Verbose:
 				tag = "VERBOSE";
 				break;
@@ -38,20 +58,10 @@ export class DbgItLoggingSink implements ILogEventSink {
 				tag = "UNKNOWN";
 				break;
 		}
-		const renderedMessage = template.Render(message);
-		const formattedMessage = `[${tag!}]: ${renderedMessage}`;
-		this.middleware.forEach((mw) => coroutine.wrap(mw)(message.Level, time, formattedMessage));
-		this.history.push({
-			severity: message.Level,
-			time: time,
-			message: formattedMessage,
-		});
-		this.history = this.history.filter(
-			(_, i) => this.history.size() > MESSAGE_HISTORY_MAX && i <= this.history.size() - MESSAGE_HISTORY_MAX,
-		);
+		return tag;
 	}
 	public addMiddleware(mw: LoggingMiddleware) {
-		this.history.forEach((v) => coroutine.wrap(mw)(v.severity, v.time, v.message));
+		this.history.forEach((v) => coroutine.wrap(mw)(v.severity, v.time, v.message, v.event));
 		this.middleware.push(mw);
 	}
 	public getMessageHistory() {
